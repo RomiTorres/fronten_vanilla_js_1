08dc77a99cdab8bc526f9041ff3109114c8f4bb0
RomiTorres <torres_rominav@hotmail.com>
2025-09-24 08:06:01 +0200

    Cuadrado cambia color con replace

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const redSquare = document.getElementById("red-square");
+const redSquare: HTMLElement = document.getElementById("red-square");
+
+redSquare.addEventListener("click", (e) => {
+  if(e.target instanceof HTMLElement){
+      if(e.target.classList.contains("red")){
+        e.target.classList.replace("red", "green");
+      } else if (e.target.classList.contains("green")) {
+           e.target.classList.replace("green", "red");
+        }
+  }
+  
+})
 
 console.log(redSquare);
