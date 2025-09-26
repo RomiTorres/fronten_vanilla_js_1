908c48d9dbb71ad6443f7397d0abd8263c721259
RomiTorres <torres_rominav@hotmail.com>
2025-09-26 07:31:00 +0200

    Añade cuadrado cuando hago click

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,24 @@
 
 
 const colorButtons = document.querySelectorAll(".color-square");
+const sendCombinationButton = document.getElementById(
+  "send-combination-button"
+);
 
 colorButtons.forEach((element) => {
-  element.addEventListener("click", () => {
+  element.addEventListener("click", (e) => {
     // crear nuevo elemento
+
+    const newCurrentCombinationButton: HTMLElement = document.createElement("div");
+
     //añadir clases del elemento clikado
+    if (e.target instanceof HTMLElement) {
+      e.target.classList.forEach((element) => {
+        newCurrentCombinationButton.classList.add(element)
+      })
+    }
     //añadir a la seccion current combination
+    sendCombinationButton.insertAdjacentElement("beforebegin", newCurrentCombinationButton);
   })
 })
 
